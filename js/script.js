38c121f57b3c9032feab97ab12c1b3c475b83dca
DANGEROUS <110321607+DANI2999@users.noreply.github.com>
2022-09-19 15:50:12 +0400

    nextworkframe1

---
+++ js/script.js
@@ -0,0 +1,36 @@
+$('.close1').on('click', function() {
+    $('.1').css({
+      opacity:'0',
+          display:'none',
+    });
+  });
+  $('.close2').on('click', function() {
+    $('.2').css({
+      opacity:'0',
+          display:'none',
+    });
+  });
+  $('.close3').on('click', function() {
+    $('.3').css({
+      opacity:'0',
+          display:'none',  
+      });
+  });
+  $('.close4').on('click', function() {
+    $('.4').css({
+      opacity:'0',
+          display:'none',
+    });
+  });
+  $('.close5').on('click', function() {
+    $('.5').css({
+      opacity:'0',
+          display:'none',
+    });
+  });
+  $('.close6').on('click', function() {
+    $('.6').css({
+      opacity:'0',
+          display:'none',
+    });
+  });
